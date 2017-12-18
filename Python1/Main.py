3f0d940bfd2a3b597d8c38573fc6d3ae3e409f34
mhmdpkts <mhmdpkts@hotmail.com>
2017-12-19 02:36:36 +0300

    Update variables

--- Python1/Main.py
+++ Python1/Main.py
@@ -5,7 +5,7 @@ import os                   # os added for cls comment
 while True :
     country = input("What is your country ? \n\n\t: ")
     print("\n\n\t",getHello.getHello(country),", Welcome my Hello Project :)")
-    exit = input("\n\n if do you want exit enter 1, please..")
+    exit = input("\n\n if do you want exit enter 1, please...")
     if(exit == '1'):
         break
     os.system("cls")
